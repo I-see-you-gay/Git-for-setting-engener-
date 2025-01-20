427f2e3b952610b9c9240419baa39b4b423771ea
Влад <125763694+I-see-you-gay@users.noreply.github.com>
2025-01-20 20:04:08 +0300

    Update task_5_2.py

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -30,3 +30,19 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip_address = input('Введите IP: ')
+ip_address = ip_address.split('.')
+prefix_mask = ip_address[-1][-2:]
+mask = int(prefix_mask) * "1" + "0" * (32 - int(prefix_mask))
+mask = '''{0:08b}{1:08b}{2:08b}{3:08b}'''.format(int(mask[:8],2), int(mask[8:16],2), int(mask[16:24],2), int(mask[24:],2))
+ip_address_host = '.'.join(ip_address)[:-3].split('.')
+ip_address_bin = '''{0:08b}{1:08b}{2:08b}{3:08b}'''.format(int(ip_address_host[0]),int(ip_address_host[1]), int(ip_address_host[2]), int(ip_address_host[3]))
+ip_address_network = ip_address_bin[:mask.count('1')]+'0'*(32-mask.count('1')) 
+print('Network:')
+print('''{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}'''.format(int(ip_address_network[0:8],2), int(ip_address_network[8:16],2), int(ip_address_network[16:24],2), int(ip_address_network[24:],2)))
+m10 = '''{0:<8}  {1:<8}  {2:<8}  {3:<8}'''.format(int(mask[0:8],2), int(mask[8:16],2), int(mask[16:24],2), int(mask[24:],2))
+mask2 = '''{0:08b}  {1:08b}  {2:08b}  {3:08b}'''.format(int(mask[:8],2), int(mask[8:16],2), int(mask[16:24],2), int(mask[24:],2))
+print('Mask:','\n'+'/'+ prefix_mask+'\n'+m10+'\n'+mask2)
+
+
