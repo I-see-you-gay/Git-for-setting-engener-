9abb2aefb78041cb1b9eda61e7f2a1a45bc61dd4
Влад <125763694+I-see-you-gay@users.noreply.github.com>
2025-01-24 23:29:31 +0300

    Update task_5_3a.py

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -12,16 +12,28 @@ VLANа или списка VLANов:
 То есть эту задачу можно решить без использования условия if и циклов for/while.
 """
 
-access_template = [
+access_template = dict(
+access = [
     "switchport mode access",
     "switchport access vlan {}",
     "switchport nonegotiate",
     "spanning-tree portfast",
     "spanning-tree bpduguard enable",
-]
+],
 
-trunk_template = [
+trunk = [
     "switchport trunk encapsulation dot1q",
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+)
+vlan_info = {
+  'access': 'Введите номер VLAN: ',
+    'trunk': 'Введите разрешенные VLANы: '
+}
+interface_mode = input("Введите режим работы интерфейса (access/trunk): ")
+interface_type = input('Введите тип и номер интерфейса: ')
+vlan_list = input(vlan_info[interface_mode]).split(',')
+print('\n'+ '-'*30)
+print('interface {}'.format(interface_type))
+print('\n'.join(access_template[interface_mode]).format(vlan_list))
